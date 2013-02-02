d8f47d7893462b00f3eb6608c08a006b13675a58
Eero af Heurlin <eero.afheurlin@iki.fi>
2013-02-03 00:22:07 +0200

    typo fixes in mappings

--- software/arduino-xmega/variants/hhl_xmega32_v1_pdi_nortc/pins_arduino.h
+++ software/arduino-xmega/variants/hhl_xmega32_v1_pdi_nortc/pins_arduino.h
@@ -61,6 +61,8 @@ USARTS
 
 #include <avr/pgmspace.h>
 
+#define USE_RTC // Use RTC for millis etc.
+
 
 #define REPEAT8(x) x, x, x, x, x, x, x, x
 #define BV0TO7 _BV(0), _BV(1), _BV(2), _BV(3), _BV(4), _BV(5), _BV(6), _BV(7)
@@ -242,10 +244,10 @@ const TC0_t* PROGMEM timer_to_tc0_PGM[] = {
 	NULL    , 
 	NULL    , 
 	NULL    , 
-	&TCA0   	,  // 17 
-	&TCA0   	, 
-	&TCA0   	, 
-	&TCA0   	, 
+	&TCC0   	,  // 17 
+	&TCC0   	, 
+	&TCC0   	, 
+	&TCC0   	, 
 	NULL   	, 
 	NULL   	, 
 	NULL	, 
