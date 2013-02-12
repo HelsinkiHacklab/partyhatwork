e179ba3612a9a003f33426353bbc0ccc2edaf585
Eero af Heurlin <eero.afheurlin@iki.fi>
2013-02-12 20:07:05 +0200

    sane default for as example, helper and documentation

--- software/pc/raw_test/xbee_test.py
+++ software/pc/raw_test/xbee_test.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python -i
 import ConfigParser, os, sys, binascii
 config = ConfigParser.SafeConfigParser()
 if not os.path.isfile('xbee.ini'):
@@ -6,7 +6,7 @@ if not os.path.isfile('xbee.ini'):
     config.set('modem', 'port', '/dev/whatever')
     config.add_section('node')
     config.set('node', 'short_addr', '0x4916')
-    config.set('node', 'long_addr', '0x13A200403AA07A')
+    config.set('node', 'long_addr', '0x0013A200403AA07A')
     with open('xbee.ini', 'wb') as configfile:
         config.write(configfile)
     print "Edit xbee.ini for your modem port"
@@ -16,6 +16,9 @@ config.read('xbee.ini')
 import logging
 logger = logging.getLogger('ZigBee')
 logger.setLevel(logging.DEBUG)
+
+def inify_address(addr)
+    return "0x%s" % binascii.hexlify(addrd).upper()
 
 
 class Unbuffered:
@@ -41,16 +44,15 @@ def cb(x):
     print x
 
 def scb(x):
-    print "Start"
-    print x
+    print "Start, got x %s" % repr(x)
 
 
 short_int = eval(config.get('node','short_addr'))
 short_daddr = pack(">H", short_int)
 long_daddr = pack('>Q',eval(config.get('node','long_addr'))) # set 64-bit address here
 
-print "short_daddr=%s\n" % repr(short_daddr)
-print "long_daddr=%s\n" % repr(long_daddr)
+print "parsed short_daddr=%s\n" % repr(short_daddr)
+print "parsed long_daddr=%s\n" % repr(long_daddr)
 
 
 ser = serial.Serial(config.get('modem', 'port'), 57600)
@@ -73,4 +75,7 @@ if 0:
     print "foo"
     data = pack('BBBB', 0, 0xff,0xff,0xff)
     xb.tx( dest_addr = short_daddr, dest_addr_long = long_daddr, data = data )
+
+
+print "use inify('adress') to dump address in format suitable for the .ini file\n"
 
