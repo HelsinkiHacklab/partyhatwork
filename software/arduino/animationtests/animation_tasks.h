013c0c636e043d48f5daeb83258515f35f47c85f
Eero af Heurlin <eero.afheurlin@iki.fi>
2013-03-09 04:22:42 +0200

    it still compiles but not tested

--- software/arduino/animationtests/animation_tasks.h
+++ software/arduino/animationtests/animation_tasks.h
@@ -4,6 +4,8 @@
 #include <Task.h>
 #include <avr/eeprom.h>
 #include <avr/pgmspace.h>
+#define FADE_MIN_TIME 10 //ms
+
 
 typedef struct {
     const void *Next; // Pointer to next animation
@@ -34,7 +36,7 @@ enum AnimationState {
     FADING_END
 };
 
-
+// Pack this matrix to a stuct to make pointer passing saner
 typedef struct {
     uint8_t leds[7][3];
 } ledtable;
@@ -52,17 +54,23 @@ public:
 
 private:
     virtual void unpack_frame(const uint8_t *start_of_frame, ledtable& tgt , uint16_t* tgt_wait_ms);
+    void interpolate_fade();
+    void set_leds(ledtable& src);
 
     ledtable leds;
-    ledtable next_leds;
-    ledtable fade_leds;
 
     const Animation* current_animation;
     uint8_t frame_size;
     uint8_t current_step;
     uint8_t num_leds;
     uint16_t wait_ms;
+
+    ledtable next_leds;
     uint16_t next_wait_ms;
+    ledtable fade_leds;
+    uint8_t fade_step;
+    uint8_t num_fade_steps;
+    uint8_t fade_wait_ms; // This will always fit to single byte, worst (and totally insane case) would be 257 as value
 
     AnimationState state;
     
@@ -164,21 +172,153 @@ void AnimationRunner::set_animation(const Animation* anim)
     set_animation(&animation_buffer);
 }
 
+void AnimationRunner::interpolate_fade()
+{
+}
+
+void AnimationRunner::set_leds(ledtable& src)
+{
+    uint8_t dummy;
+    for (uint8_t i=0; i < 8; i++)
+    {
+        if (current_animation->leds & _BV(i))
+        {
+            for (uint8_t ii=0; i < 3; i++)
+            {
+                dummy = src.leds[i][ii];
+            }
+        }
+    }
+}
 
 void AnimationRunner::run(uint32_t now)
 {
     Serial.print(F("Running step "));
     Serial.println(current_step, DEC);
 
-    // TODO: Set the LED values
-
-    incRunTime(wait_ms);
-    current_step++;
-    if (current_step >= current_animation->length)
+    switch (state)
     {
-        current_step = 0;
+        case RUNNING:
+        {
+            set_leds(leds);
+          
+            if (current_animation->modes == 0x1) // Fade mode animation
+            {
+                state = FADING_START;
+                // Returning without incruntime means we will immediately (well, on next scheduler iteration but that's gonne be real-soon-now) come back to the next state
+                return;
+            }
+
+
+            // The default hard-switch handler, load next frame and wait.
+            incRunTime(wait_ms);
+            current_step++;
+            if (current_step >= current_animation->length)
+            {
+                current_step = 0;
+            }
+            unpack_frame(current_animation->first_frame+(current_step*frame_size), leds, &wait_ms);
+
+            break;
+        }
+        // This is porbably unneeded
+        case FADING_START:
+        {
+            // Load next frame (looping as neccessary
+            uint8_t next_step = current_step+1;
+            if (next_step >= current_animation->length)
+            {
+                next_step = 0;
+            }
+            unpack_frame(current_animation->first_frame+(next_step*frame_size), next_leds, &next_wait_ms);
+            // Figure out step time and max number of steps we can take within the constraints
+            fade_wait_ms = FADE_MIN_TIME;
+            uint16_t fade_max_steps = wait_ms/FADE_MIN_TIME;
+            if (fade_max_steps > 255)
+            {
+                fade_max_steps = 255;
+                fade_wait_ms = wait_ms/255;
+            }
+            // Set next runtime before all the heavy calculcations
+            incRunTime(fade_wait_ms);
+
+            // Calculate largest difference between values to interpolate
+            uint8_t largest_diff = 0;
+            for (uint8_t i=0; i < 8; i++)
+            {
+                if (current_animation->leds & _BV(i))
+                {
+                    for (uint8_t ii=0; ii < 3; ii++)
+                    {
+                        uint8_t tmpval1 = leds.leds[i][ii];
+                        uint8_t tmpval2 = next_leds.leds[i][ii];
+                        uint8_t tmpdiff;
+                        if (tmpval1 > tmpval2)
+                        {
+                            tmpdiff = tmpval1-tmpval2;
+                        }
+                        else
+                        {
+                            tmpdiff = tmpval2-tmpval1;
+                        }
+                        if (tmpdiff > largest_diff)
+                        {
+                            largest_diff = tmpdiff;
+                        }
+                    }
+                }
+            }
+            // Set the number of steps we are going to fade for
+            if (largest_diff <= fade_max_steps)
+            {
+                num_fade_steps = largest_diff;
+            }
+            else
+            {
+                num_fade_steps = fade_max_steps;
+            }
+            fade_step = 0;
+            interpolate_fade();
+            state = FADING;
+            break;
+        }
+        case FADING:
+        {
+            set_leds(fade_leds);
+
+            // Set next runtime before all the heavy calculcations
+            incRunTime(fade_wait_ms);
+            if (fade_step >= num_fade_steps)
+            {
+                state = FADING_END;
+            }
+            fade_step++;
+            interpolate_fade();
+
+            break;
+        }
+        case FADING_END:
+        {
+            set_leds(fade_leds);
+            
+            incRunTime(fade_wait_ms);
+            state = RUNNING;
+
+            current_step++;
+            if (current_step >= current_animation->length)
+            {
+                current_step = 0;
+            }
+            //unpack_frame(current_animation->first_frame+(current_step*frame_size), leds, &wait_ms);
+            // We already unpacked the next frame to calculate the interpolations, so just switch them over.
+            leds = next_leds;
+            wait_ms = next_wait_ms;
+            
+            break;
+        }
     }
-    unpack_frame(current_animation->first_frame+(current_step*frame_size), leds, &wait_ms);
+
+
 }
 
 
