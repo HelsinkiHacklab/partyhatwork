191c373f4cd0d13b9954cfbbc556246ac3d570fe
Eero af Heurlin <eero.afheurlin@iki.fi>
2013-03-16 20:34:18 +0200

    looking very good

--- hardware/mug_diffuser_1/curve_fitting.py
+++ hardware/mug_diffuser_1/curve_fitting.py
@@ -21,8 +21,8 @@ ax.plot(measured_xs, measured_ys, 'ro')
 # Bezier code from http://matplotlib.org/users/path_tutorial.html
 verts = [
     measured_points[0],  # P0
-    (63, 40), # P1
-    (97, 28), # P2
+    (62, 41), # P1
+    (97, 27), # P2
     measured_points[1], # P3
     ]
 
