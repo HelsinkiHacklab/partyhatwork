012f2aca992be2f2f3462522aa7f37f50c98ca7c
Eero af Heurlin <eero.afheurlin@iki.fi>
2013-09-03 19:32:08 +0300

    clarified documentation

--- software/pc/beehive/node.py
+++ software/pc/beehive/node.py
@@ -16,10 +16,11 @@ class XbeeNode(object):
         self.node_identifier = kwargs['node_identifier']
 
     def tx(self, *args):
-        """Send data to target node, each argument is single byte to send (if you have a tuple mydata you can pass it as arguments with *mydata"""
+        """Send data to target node, each argument is single byte to send (if you have a tuple/list mydata you can pass it as arguments with *mydata"""
         data_packed = pack("%dB" % len(args), *args)
         self.xb.tx( dest_addr = self.short_addr, dest_addr_long = self.long_addr, data = data_packed )
 
     def tx_string(self, send_string):
+        """Send a string (ASCII) to node, this will handle unpacking of the string to list of bytes and passing it correctly"""
         send_args = [ ord(x) for x in send_string ]
         self.tx(*send_args)
